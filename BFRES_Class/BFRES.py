42d5f6e030f93c3ec57062f35abc35e32c9d9a35
smb123w64gb <smb123w64gb@gmail.com>
2016-10-21 00:08:55 -0700

    Added Header Class Beginings

--- BFRES_Class/BFRES.py
+++ BFRES_Class/BFRES.py
@@ -1,13 +1,25 @@
 import sys
 from util import *
 
-f = open("test.bfres", "wb")
-
-f.write("FRES")
-write32be(f,0x03040002)
-write32be(f,0xFFFF0010)
-f.seek(0x10)#Size Later on @ 0xC
-write32be(f,0x00001000)
-
-
-f.close()
+class Header(object):
+    def __init__(self):
+        self.magic = "FRES"
+        self.topVertion = 3
+        self.botVertion = 4
+        self.boneVertion = 4
+        self.endian = 0xFEFF
+        self.unk0 = 0x10
+        self.alinement = 0x2000
+        self.fileName = "Test"
+    def write(self,file):
+        file.write(self.magic)
+        writeByte(file,self.topVertion)
+        writeByte(file,self.botVertion)
+        write16be(file,self.boneVertion)
+        write16be(file,self.endian)
+        write16be(file,self.unk0)
+        file.seek(4,1)
+        #add a bit for adding new stringPoiterEntry!
+        file.seek(0x5C,1)
+        
+        
